43d982b979f33e0ee9b0fcf4776457b694799b92
Student <Student@lex-desk-4>
2025-03-05 12:16:07 +0100

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static int Add(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+        public static int Substract(params int[] numbers)
+        {
+            if (numbers.Length == 0) return 0;
+
+            int result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result -= numbers[i];
+            }
+            return result;
+        }
+        
+        
+        public static int Multiply(int num1 , int num2)
+        {
+            
+            return num1*num2;
+        }
+        public static float Divide(float num1 , float num2)
+        {
+            
+            return num1/num2;
+        }
+        
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.ComponentModel;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int>();
+            string input;
+
+            while (true)
+            {
+                Console.WriteLine("Please choose opertaion to follow add,subtract,multiply or divide, you may write 'exit' to end the application");
+
+                string operation = Console.ReadLine();
+
+                if (operation.ToLower() == "add" || operation.ToLower() == "subtract")
+                {
+
+                    while (true)
+                    {
+                        Console.WriteLine("Please enter numbers to be calculated based on the choosen operation. Type 'calculate' to perform the operation.");
+                        input = Console.ReadLine();
+
+                        if (input.ToLower() == "calculate")
+                        {
+                            break;
+                        }
+
+                        if (int.TryParse(input, out int number))
+                        {
+                            numbers.Add(number);
+                            Console.WriteLine($"Number {number} is number {numbers.Count} in the list!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter a valid number or type 'calculate' to perform the operation.");
+                        }
+                    }
+
+                    int sum = 0;   
+                    if(operation.ToLower() == "add")
+                    {
+                        sum = Calculator.Add(numbers.ToArray());
+                    }
+                    else if (operation.ToLower() == "subtract")
+                    {
+                        sum = Calculator.Substract(numbers.ToArray());
+                    }
+                   
+
+                    Console.WriteLine($"the sum is {sum}");
+                    numbers.Clear();
+
+
+                }
+          
+                else if (operation.ToLower() == "multiply" || operation.ToLower() == "divide")
+                {
+                    Console.WriteLine("please enter the first number");
+                    string input1 = Console.ReadLine();
+                    Console.WriteLine("please enter the second number");
+                    string input2 = Console.ReadLine();
+
+                    float result = 0;
+                    if (int.TryParse(input1, out int num1) && int.TryParse(input2, out int num2))
+                    {
+                      result =  operation.ToLower() == "multiply" ? result = Calculator.Multiply(num1, num2)
+                        : result = Calculator.Divide(num1, num2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid inputs. Please enter a valid number.");
+                    }
+                    Console.WriteLine($"the sum is {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation. Please choose a valid operation.");
+
+                }
+
+                if (operation.ToLower() == "exit")
+                {
+                    break;
+                }
+
+
+            }
+        }
+    }
+}
